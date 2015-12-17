445621b7a15978ac2569d62ff751f23ef0bc35e9
Samantha Parra <samantha.parra01@gmail.com>
2015-12-17 11:41:16 -0800

    first upload

--- app/build/generated/source/r/debug/com/example/android/vinsonjustjava/R.java
+++ app/build/generated/source/r/debug/com/example/android/vinsonjustjava/R.java
@@ -1590,8 +1590,8 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020042;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020043;
         public static final int abc_textfield_search_material=0x7f020044;
-        public static final int javalogo=0x7f020045;
         public static final int notification_template_icon_bg=0x7f020046;
+        public static final int world=0x7f020045;
     }
     public static final class id {
         public static final int action0=0x7f0c0058;
@@ -1748,7 +1748,8 @@ containing a value of this type.
         public static final int menu_main=0x7f0d0000;
     }
     public static final class mipmap {
-        public static final int ic_launcher=0x7f030000;
+        public static final int coffee=0x7f030000;
+        public static final int ic_launcher=0x7f030001;
     }
     public static final class string {
         public static final int abc_action_bar_home_description=0x7f060000;
